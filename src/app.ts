e1cae87e5211fa1293cfc0951085fbe756af697d
Idowu-David <Idowu-David@github.com>
2026-04-14 12:33:17 +0100

    add: vercel file for deploy

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("REQUEST CHECK");
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/classify", classifyRoute);
 
