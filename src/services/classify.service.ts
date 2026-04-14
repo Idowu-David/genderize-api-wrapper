2dc6887dd7ace81cd6a5129ae8b8e4840776811f
Idowu-David <Idowu-David@github.com>
2026-04-14 19:46:55 +0100

    fix: timeout and params on axios

--- src/services/classify.service.ts
+++ src/services/classify.service.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const fetchClassifyData = async (name: string) => {
-  const classify = await axios.get(`https://api.genderize.io?name=${name}`);
+  const classify = await axios.get("https://api.genderize.io", {
+    params: { name },
+    timeout: 3000,
+  });
   return classify.data;
 };
